0398859a42d089b5b6de76ebd7851e67a8e0bedd
Harsh Kumar <harsh007kumar@gmail.com>
2020-05-03 01:12:59 +0530

    support added for processing IPL  Data

---
+++ COVID_Data_Filtration/CommonUtility.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COVID_Data_Filtration
+{
+    class CommonUtility
+    {
+        /// <summary>
+        /// Read no of line from text file and returns the count as int value
+        /// </summary>
+        /// <param name="textFilePath"></param>
+        /// <returns></returns>
+        public static int ReadNoOfLines(string textFilePath)
+        {
+            using (StreamReader file = new StreamReader(textFilePath))
+            {
+                int noOfLines = 0;
+                while (file.ReadLine() != null)
+                    noOfLines++;
+                file.Close();
+                Console.WriteLine($"File has {noOfLines} lines.");
+                return noOfLines;
+            }
+        }
+
+        /// <summary>
+        /// Write the array of strings in specified output file, each string index is stored line by line
+        /// </summary>
+        /// <param name="sourceFile"></param>
+        /// <param name="outputBuffer"></param>
+        public static void WriteToOutputFile(string sourceFile, string[] outputBuffer)
+        {
+            File.WriteAllLines(sourceFile, outputBuffer);
+        }
+
+        public static string[] FetchContentsOfTextFile(string textFilePath)
+        {
+            using (StreamReader file = new StreamReader(textFilePath))
+            {
+                int noOfLines = 0;
+                string line;
+                List<string> lines = new List<string>();
+                while ((line = file.ReadLine()) != null)
+                {
+                    //string[] fields = line.Split('\t');
+                    lines.Add(line);
+                    noOfLines++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {noOfLines} lines.");
+                return lines.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Function which take string input and splits it using passed seperator and return int array of numbers present in string
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="splitby"></param>
+        /// <returns></returns>
+        public static int[] ConvertLineToIntArray(string line, char splitby='\t')
+        {
+            string[] fields = line.Split(splitby);
+            int[] array = new int[fields.Length-1];
+            for (int i = 1; i < fields.Length; i++)
+                array[i-1] = Convert.ToInt32(fields[i]);
+            return array;
+        }
+
+        /// <summary>
+        /// Function which take int array of numbers snf returns string with values seperated by '\t'
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="splitby"></param>
+        /// <returns></returns>
+        public static string ConvertIntArrayToLine(int[] array, char splitby = '\t')
+        {
+            StringBuilder line = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+                line.Append(array[i]).Append('\t');
+            return line.ToString();
+        }
+
+        /// <summary>
+        /// For Input 1,1,1,1,1 return fibonacci way of sum like 1,2,3,4,5 as Output
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int[] SumInFibonaciSeries(int[] array)
+        {
+            for (int index = 1; index < array.Length; index++)
+                array[index] += array[index - 1];
+            return array;
+        }
+
+        public static string[] removeIndex(string[] array, int indexToBeRemoved = 0)
+        {
+            int len;
+            if ((len = array.Length) == 1)
+                return new string[0];
+            else
+            {
+                string[] newArray = new string[len - 1];
+                for (int i = 1; i < len; i++)
+                    newArray[i - 1] = array[i];
+                return newArray;
+            }
+        }
+
+    }
+}
--- COVID_Data_Filtration/Program.cs
+++ COVID_Data_Filtration/Program.cs
@@ -1,27 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using COVID_Data_Filtration;
 
 namespace COVID_Data_Filtration
 {
     public class Covid
     {
         static string tabSpace= "	";
-        public static int ReadNoOfLines(string textFilePath)
-        {
-            using (StreamReader file = new StreamReader(textFilePath))
-            {
-                int noOfLines = 0;
-                while (file.ReadLine() != null)
-                    noOfLines++;
-                file.Close();
-                Console.WriteLine($"File has {noOfLines} lines.");
-                return noOfLines;
-            }
-        }
         public static void FindNoOfInfectedStatesAndUniqueDates(string textFilePath, ref List<string> infectedStates,ref List<DateTime> uniqueDates, ref StringBuilder noOfColumns)
         {
             using (StreamReader file = new StreamReader(textFilePath))
@@ -92,6 +79,7 @@ namespace COVID_Data_Filtration
                     }
                     lineNo++;
                 }
+                FlushDataTillLastDate(ref MasterData_State, CurrentDate_Data);          // add final date entry to master data.
                 file.Close();
                 Console.WriteLine($"File has {lineNo} lines.");
             }
@@ -101,22 +89,124 @@ namespace COVID_Data_Filtration
         static void Main(string[] args)
         {
             Console.WriteLine("========================== START {0} ========================", DateTime.UtcNow);
-            string SourceFile = "..\\..\\SourceFile.txt";
-            string OutputFile = "..\\..\\OutputFile.txt";
-            StringBuilder noOfColumn = new StringBuilder();
-            int noOfLines = ReadNoOfLines(SourceFile);
-            List<DateTime> uniqueDates = new List<DateTime>(noOfLines);             // Generic List of type DateTime
-            List<string> infectedStates = new List<string>(noOfLines);              // Generic List of type string
-            Patient[] plist = new Patient[noOfLines];                               // List of Patients
-            FindNoOfInfectedStatesAndUniqueDates(SourceFile,ref infectedStates, ref uniqueDates, ref noOfColumn);
-            // Processing Data
-            Dictionary<string,string> FormattedData = ProcessTxtFile(SourceFile,ref plist, ref infectedStates);
+            //string SourceFile = "..\\..\\SourceFile.txt";
+            //string OutputFile = "..\\..\\OutputFile.txt";
+            //StringBuilder noOfColumn = new StringBuilder();
+            //int noOfLines = CommonUtility.ReadNoOfLines(SourceFile);
+            //List<DateTime> uniqueDates = new List<DateTime>(noOfLines);             // Generic List of type DateTime
+            //List<string> infectedStates = new List<string>(noOfLines);              // Generic List of type string
+            //Patient[] plist = new Patient[noOfLines];                               // List of Patients
+            //FindNoOfInfectedStatesAndUniqueDates(SourceFile,ref infectedStates, ref uniqueDates, ref noOfColumn);
+            //// Processing Data
+            //Dictionary<string,string> FormattedData = ProcessTxtFile(SourceFile,ref plist, ref infectedStates);
+            //string[] outputBuffer = new string[infectedStates.Count + 1];           // array to hold each state data plus column fields
+            //PrepareOutputBuffer(ref outputBuffer, noOfColumn, FormattedData);       // created Output buffer which is array of strings from FormattedData dictonary
+            //CommonUtility.WriteToOutputFile(OutputFile, outputBuffer);                            // Flush data
 
-            string[] outputBuffer = new string[infectedStates.Count + 1];           // array to hold each state data plus column fields
-            PrepareOutputBuffer(ref outputBuffer, noOfColumn, FormattedData);       // created Output buffer which is array of strings from FormattedData dictonary
-            WriteToOutputFile(OutputFile, outputBuffer);                            // Flush data
-            Console.WriteLine("========================== END {0} ==========================", DateTime.UtcNow);
-            Console.ReadKey();
+
+
+            //string SourceFile2 = "..\\..\\SourceFile2.txt";
+            //string OutputFile2 = "..\\..\\OutputFile2.txt";
+            //string[] contents = CommonUtility.FetchContentsOfTextFile(SourceFile2);
+            //string[] toProcess = CommonUtility.removeIndex(contents);
+            //string[] outputBuffer = new string[contents.Length - 1];
+            //for (int i = 1; i < contents.Length; i++)
+            //{
+            //    int[] lineArray = CommonUtility.ConvertLineToIntArray(contents[i]);
+            //    lineArray = CommonUtility.SumInFibonaciSeries(lineArray);
+            //    outputBuffer[i - 1] = CommonUtility.ConvertIntArrayToLine(lineArray);
+            //}
+            //CommonUtility.WriteToOutputFile(OutputFile2, outputBuffer);                            // Flush data
+            //Console.WriteLine("========================== END {0} ==========================", DateTime.UtcNow);
+            //Console.ReadKey();
+
+
+
+            // ====================== IPL Data ============================= //
+            string SourceFile = "..\\..\\IPL_Data.txt";
+            string OutputFile = "..\\..\\IPL_OutPut.txt";
+            StringBuilder FirstLine = new StringBuilder();
+            int noOfLines = CommonUtility.ReadNoOfLines(SourceFile);
+            List<DateTime> uniqueDates = new List<DateTime>();                      // Generic List of type DateTime
+            List<string> teams = new List<string>();                                // Generic List to fetch 2nd primary key/unique key in input file
+            Dictionary<string, string> teamsData = new Dictionary<string, string>();
+            FetchNoOfUniqueKeysAndDates(SourceFile, ref teams, ref uniqueDates, ref FirstLine);
+            // Processing Data
+            Dictionary<string,string> FormattedData = SeasonStatsForTeams(SourceFile, ref teamsData, uniqueDates, teams);
+
+            string[] outputBuffer = new string[teams.Count + 1];                    // array to hold each team data plus column fields
+            PrepareOutputBuffer(ref outputBuffer, FirstLine, FormattedData);        // created Output buffer which is array of strings from FormattedData dictonary
+            CommonUtility.WriteToOutputFile(OutputFile, outputBuffer);              // Flush data
+        }
+
+        private static void FetchNoOfUniqueKeysAndDates(string textFilePath, ref List<string> teams, ref List<DateTime> uniqueDates, ref StringBuilder firstLine)
+        {
+            using (StreamReader file = new StreamReader(textFilePath))
+            {
+                bool readLine = false;
+                string line;
+                DateTime dt;
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] fields = line.Split('\t');
+                    if (readLine)
+                    {
+                        if (!teams.Contains(fields[0]))
+                            teams.Add(fields[0]);                                          // Append unique states to List
+                        if (!uniqueDates.Contains(Convert.ToDateTime(fields[1])))
+                        {
+                            dt = Convert.ToDateTime(fields[1]);
+                            uniqueDates.Add(dt);                                                    // Append unique dates to list
+                            firstLine.Append(tabSpace).Append(dt.ToString("dd/MMM/yyyy"));        // Append unique dates to List of Columns
+                        }
+                    }
+                    else
+                        firstLine.Append(fields[1]);                                              // Append existing columns from text file 1st line
+                    readLine = true;
+                }
+                file.Close();
+            }
+        }
+
+        private static Dictionary<string, string> SeasonStatsForTeams(string sourceFile, ref Dictionary<string, string> teamsWinData, List<DateTime> uniqueDates, List<string> teams)
+        {
+            Dictionary<string, string> MasterData = new Dictionary<string, string>();       // Final Dictonary which is being flushed in Output text file
+            Dictionary<string, int> CurrentDate_Data = new Dictionary<string, int>();       // Dictonary to hold values of team win on a particular date.
+            InitializeTeams(ref CurrentDate_Data, teams);                                   // Reset All Teams Data to Zero
+            using (StreamReader file = new StreamReader(sourceFile))
+            {
+                int lineNo = 0;
+                string line, previousDate = "", teamName, currentDate;
+                bool skipOnce = false;
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] fields = line.Split('\t');
+                    teamName = fields[0];
+                    currentDate = fields[1];
+                    if (lineNo > 0)
+                    {
+                        if (currentDate != previousDate)
+                        {
+                            if (skipOnce)
+                                FlushDataTillLastDate(ref MasterData, CurrentDate_Data); skipOnce = true;
+                            previousDate = currentDate;
+                        }
+                        CurrentDate_Data[teamName] += 1;
+                    }
+                    lineNo++;
+                }
+                FlushDataTillLastDate(ref MasterData, CurrentDate_Data);                    // add final date entry to master data.
+                file.Close();
+                Console.WriteLine($"File has {lineNo} lines.");
+            }
+            return MasterData;
+        }
+
+        private static void InitializeTeams(ref Dictionary<string, int> currentDate_Data, List<string> teams)
+        {
+            foreach (string team in teams)
+                if (!currentDate_Data.ContainsKey(team))
+                    currentDate_Data.Add(team, 0);                                                  // add team if not present
         }
 
         private static void PrepareOutputBuffer(ref string[] outputBuffer, StringBuilder noOfColumn, Dictionary<string, string>formattedData)
@@ -125,11 +215,6 @@ namespace COVID_Data_Filtration
             int i = 1;
             foreach (KeyValuePair<string, string> line in formattedData)
                 outputBuffer[i++] = line.Key + tabSpace + line.Value;
-        }
-
-        private static void WriteToOutputFile(string sourceFile, string[] outputBuffer)
-        {
-            File.WriteAllLines(sourceFile, outputBuffer);
         }
 
         // update Data for each state till current date
